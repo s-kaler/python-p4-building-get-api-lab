4cbf57fc320a326f33ff3822610739d72a265b10
Sundeep <sundeep.kaler@gmail.com>
2024-10-20 06:58:18 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -20,19 +20,47 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakeries_formatted =  []
+    for bakery in bakeries:
+        bakery_formatted = {'id': bakery.id,
+                            'name': bakery.name,
+                            'created_at': bakery.created_at,
+                            'updated_at': bakery.updated_at}
+        bakeries_formatted.append(bakery_formatted)
+    
+    return make_response(bakeries_formatted, 200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery_formatted = {'id': bakery.id,
+                            'name': bakery.name,
+                            'created_at': bakery.created_at,
+                            'updated_at': bakery.updated_at}
+    return make_response(bakery_formatted, 200)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_formatted = []
+    for good in baked_goods:
+        baked_goods_formatted.append({'id': good.id,
+                                        'name': good.name,
+                                        'price': good.price,
+                                        'created_at': good.created_at,
+                                        'updated_at': good.updated_at})
+    return make_response(baked_goods_formatted, 200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    baked_good_formatted = {'id': good.id,
+                            'name': good.name,
+                            'price': good.price,
+                            'created_at': good.created_at,
+                            'updated_at': good.updated_at}
+    return make_response(baked_good_formatted, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
